ec2a081599dfd35b097563f99ce9d9adf3cea381
DivyRaj <divyraj65407@gmail.com>
2021-08-08 21:29:35 +0530

    UI Improved

--- app/src/main/java/com/example/twisterchat/MessagesAdapter.java
+++ app/src/main/java/com/example/twisterchat/MessagesAdapter.java
@@ -123,6 +123,4 @@ public class MessagesAdapter extends RecyclerView.Adapter {
     }
 
 
-
-
 }
--- app/src/main/java/com/example/twisterchat/chatActivity.java
+++ app/src/main/java/com/example/twisterchat/chatActivity.java
@@ -1,24 +1,22 @@
 package com.example.twisterchat;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.viewpager.widget.ViewPager;
-
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -45,8 +43,6 @@ public class chatActivity extends AppCompatActivity {
 
         tabLayout=findViewById(R.id.include);
         mchat=findViewById(R.id.chat);
-        mcall=findViewById(R.id.calls);
-        mstatus=findViewById(R.id.status);
         viewPager=findViewById(R.id.fragmentcontainer);
 
         firebaseFirestore=FirebaseFirestore.getInstance();
